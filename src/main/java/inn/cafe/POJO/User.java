914290f4c3c5dbf1b9e523348761812ac887900c
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2025-02-02 19:46:46 +0530

    added User POJO

---
+++ src/main/java/inn/cafe/POJO/User.java
@@ -0,0 +1,44 @@
+package inn.cafe.POJO;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.io.Serializable;
+
+@Entity
+@DynamicUpdate
+@DynamicInsert
+@Data
+@NoArgsConstructor
+@Table(name = "user")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "contact_number")
+    private String contactNumber;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "role")
+    private String role;
+
+}
